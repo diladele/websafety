d7e4768c07d12b93dc12cf575099b245f92066bb
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-07-29 14:38:54 +0200

    ClamAv.cc patch

--- scripts.debian10/07_clamav.sh
+++ scripts.debian10/07_clamav.sh
@@ -9,37 +9,23 @@ fi
 # install clamav
 apt-get install -y clamav clamav-daemon libclamav-dev
 
-# we will be working in a subfolder
-rm -R ./build/ecap_clamav
-mkdir -p ./build/ecap_clamav
-
-# change into the folder
-pushd ./build/ecap_clamav
-
 # from now on every error is fatal
 set -e
 
 # download the sources
 wget http://www.e-cap.org/archive/ecap_clamav_adapter-2.0.0.tar.gz
 
-# unpack and untar them
-gunzip ecap_clamav_adapter-2.0.0.tar.gz
-tar -xvf ecap_clamav_adapter-2.0.0.tar
-
-# change working dir
-pushd ecap_clamav_adapter-2.0.0
-
-# configure
-./configure
+# unpack
+tar -xvzf ecap_clamav_adapter-2.0.0.tar.gz
 
 # patch the CL_SCAN_STDOPT error
-patch patch /etc/default/c-icap < ClamAv.cc.patch
+patch ecap_clamav_adapter-2.0.0/src/ClamAv.cc < ClamAv.cc.patch
 
-# make 
-make && make install
+# change into working dir
+pushd ecap_clamav_adapter-2.0.0
+
+# build
+./configure && make && make install
 
 # revert back
-popd
-
-# and again
 popd
