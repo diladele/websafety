2f3b6619d6f1f4d570bd81b89b83bc08407b79c3
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-10-08 13:05:37 +0200

    renamed path

--- scripts.ubuntu18/04_websafety.sh
+++ scripts.ubuntu18/04_websafety.sh
@@ -12,7 +12,7 @@ MINOR="878F"
 ARCH="amd64"
 
 # download
-wget http://packages.diladele.com/websafety-core/$MAJOR.$MINOR/$ARCH/release/ubuntu18/websafety-$MAJOR.${MINOR}_$ARCH.deb
+wget http://packages.diladele.com/websafety/$MAJOR.$MINOR/$ARCH/release/ubuntu18/websafety-$MAJOR.${MINOR}_$ARCH.deb
 
 # install
 dpkg --install websafety-$MAJOR.${MINOR}_$ARCH.deb
