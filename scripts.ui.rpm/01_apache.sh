e5eaa0dd57727e982e8effb34dcb9a6d8d9f26b8
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-11-08 18:22:28 +0100

    added initial ui.rpm

---
+++ scripts.ui.rpm/01_apache.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# all web packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install apache web server and required tools
+dnf -y install httpd httpd-devel krb5-workstation mc
+
+# make apache autostart on reboot
+systemctl enable httpd.service
+
+# install python 3 libs and compiler (needed for reportlab and pandas)
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y python36u python36u-pip python36u-devel
+
+# create symlinks for python 3.6 and pip3
+ln -s /usr/bin/python3.6 /usr/bin/python3
+ln -s /usr/bin/pip3.6 /usr/bin/pip3
+
+# install python django for web ui
+pip3 install django==2.1.2
+pip3 install pytz
+pip3 install requests
+pip3 install PyYAML
+pip3 install PyOpenSSL
+pip3 install pandas
+pip3 install mod-wsgi
+pip3 install reportlab==3.4.0
+pip3 install python-ldap
+
+# enable the mod_wsgi module for python3 in apache
+mod_wsgi-express install-module > /etc/httpd/conf.modules.d/02-wsgi.conf
+
+# and restart apache
+service httpd restart
+
+# allow connection to 80, 443 and 3128 ports for apache and squid
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
