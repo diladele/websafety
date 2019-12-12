d11b3bbecd96f85a6a7d826148794c830109b6a2
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-12-12 10:16:50 +0100

    fixing ui generation

--- ui.deb/02_webui.sh
+++ ui.deb/02_webui.sh
@@ -27,7 +27,8 @@ dpkg --install websafety-ui-$MAJOR.${MINOR}_$ARCH.deb
 sudo -u websafety python3 /opt/websafety-ui/var/console/utils.py --network=$OSNAME
 
 # sync ui and actual files in disk
-sudo -u proxy python3 /opt/websafety-ui/var/console/generate.py
+sudo -u proxy python3 /opt/websafety-ui/var/console/generate.py --core
+sudo -u websafety python3 /opt/websafety-ui/var/console/generate.py --ui
 
 # relabel folder
 chown -R websafety:websafety /opt/websafety-ui
--- ui.rpm/02_webui.sh
+++ ui.rpm/02_webui.sh
@@ -19,7 +19,8 @@ curl -O http://packages.diladele.com/websafety-ui/$MAJOR.$MINOR/$ARCH/release/ce
 dnf -y install websafety-ui-${MAJOR}-${MINOR}.x86_64.rpm
 
 # sync ui and actual files in disk
-sudo -u squid python3 /opt/websafety-ui/var/console/generate.py
+sudo -u squid python3 /opt/websafety-ui/var/console/generate.py --core
+sudo -u websafety python3 /opt/websafety-ui/var/console/generate.py --ui
 
 # relabel folder
 chown -R websafety:websafety /opt/websafety-ui
