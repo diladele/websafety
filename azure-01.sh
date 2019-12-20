953625e71ea672efc417a9c00a011c071670ab66
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-12-20 08:06:57 +0100

    added azure scripts

---
+++ azure-01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# check we are root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+pushd core.ubuntu18 && bash 01_update.sh && popd
---
+++ azure-02.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# check we are root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install core modules of web safety
+pushd core.ubuntu18
+bash 02_squid.sh && \
+bash 03_clamav.sh && \
+bash 04_websafety.sh && \
+bash 05_integrate.sh
+popd
+
+# install web safety ui
+pushd ui.deb
+bash 01_apache.sh && bash 02_webui.sh
+popd
+
+# tell 
+echo "SUCCESS"
+echo "SUCCESS"
+echo "SUCCESS --- Azure VM is ready ---"
+cat /opt/websafety/etc/license.pem | grep "Not After"
+echo "SUCCESS"
+echo "SUCCESS"
