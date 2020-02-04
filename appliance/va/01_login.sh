859eaeb2bd97c9a0a43d12a39084da8df107fb7a
Rafael Akchurin <rafael.akchurin@diladele.com>
2020-02-04 12:16:21 +0100

    va has squid logs stored for 1 year

--- appliance/va/01_login.sh
+++ appliance/va/01_login.sh
@@ -35,4 +35,7 @@ cp wsissue.service /etc/systemd/system/wsissue.service
 # enable it
 systemctl enable wsissue.service
 
+# set the logrotate settings to keep last 365 days of logs
+cp squid /etc/logrotate.d/ && chown root:root /etc/logrotate.d/squid && chmod 644 /etc/logrotate.d/squid
+
 echo "Success, run next step please."
