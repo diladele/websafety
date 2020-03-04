8363ba68342f86e1cfbe3368a886e7c8966d351a
Rafael Akchurin <rafael.akchurin@diladele.com>
2020-03-04 11:06:53 +0100

    new 7.3

--- ui.deb/02_webui.sh
+++ ui.deb/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default arch and version
 MAJOR="7.3.0"
-MINOR="86AB"
+MINOR="E3B2"
 ARCH="amd64"
 
 # default os
--- ui.rpm/02_webui.sh
+++ ui.rpm/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default params
 MAJOR="7.3.0"
-MINOR="86AB"
+MINOR="E3B2"
 ARCH="amd64"
 OSNAME="centos8"
 
