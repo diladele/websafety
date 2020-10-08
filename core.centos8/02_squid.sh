5ae4bd70040438a8c18b563015f139ec831b2cdb
Rafael Akchurin <rafael.akchurin@diladele.com>
2020-10-08 12:17:44 +0200

    centos gotcha

--- core.centos8/02_squid.sh
+++ core.centos8/02_squid.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# install stock squid with ecap libs
+# install stock squid with ecap libs - look for gotcha at https://github.com/diladele/websafety/issues/1300
 dnf -y install squid libecap libecap-devel
 
 # make squid autostart after reboot
