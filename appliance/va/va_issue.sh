484a11f3376f866df309c16fd980dde9bf4138b1
Rafael Akchurin <rafael.akchurin@diladele.com>
2021-11-15 09:36:51 +0100

    updated

--- appliance/va/va_issue.sh
+++ appliance/va/va_issue.sh
@@ -28,8 +28,8 @@ echo "Hard Disk Size      $DISKSZ"
 echo
 echo "Appliance Version   $VA_VER"
 echo "Squid Version       $SQUID_VER"
-echo "Default Username    root"
-echo "Default Password    Passw0rd"
+echo "System Username     root"
+echo "System Password     Passw0rd (or whatever you set it to when creating this appliance)"
 echo "Installation Dir    /opt/websafety"
 echo 
 if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]
@@ -37,12 +37,12 @@ then
     echo "To use this Virtual Appliance - adjust your browser proxy settings to point"
     echo "to the IP address or domain name of this box (port 3128) and browse the web."
     echo 
-    echo "Full featured Administrators Web Console is available at this box port 80"
+    echo "Admin UI is available at this machine IP address, port 80"
 else
     echo "To use this Virtual Appliance - adjust your browser proxy settings to point"
-    echo "to the IP address or domain name of this box \\4, port 3128 and browse the web."
+    echo "to the IP address or domain name of this machine \\4, port 3128 and browse the web."
     echo 
-    echo "Full featured Administrators Web Console is available at http://\\4:80"
+    echo "Admin UI is available at http://\\4:80"
 fi
 echo 
 echo    
