5d30a494931fdd8a944eb73c354b70dc1bab5288
Rafael Akchurin <rafael.akchurin@diladele.com>
2021-11-15 10:09:15 +0100

    8.0

--- ui.deb/02_webui.sh
+++ ui.deb/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default arch and version
 MAJOR="8.0.0"
-MINOR="98B7"
+MINOR="F600"
 ARCH="amd64"
 
 # default os
--- ui.rpm/02_webui.sh
+++ ui.rpm/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default params
 MAJOR="8.0.0"
-MINOR="98B7"
+MINOR="F600"
 ARCH="amd64"
 OSNAME="redhat8"
 
