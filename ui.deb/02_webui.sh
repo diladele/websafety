ed90716427d7a426efa7edf691c240a0e5c88053
Rafael Akchurin <rafael.akchurin@diladele.com>
2022-01-16 12:17:35 +0100

    updated

--- ui.deb/02_webui.sh
+++ ui.deb/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default arch and version
 MAJOR="8.0.0"
-MINOR="4279"
+MINOR="572F"
 ARCH="amd64"
 
 # default os
--- ui.rpm/02_webui.sh
+++ ui.rpm/02_webui.sh
@@ -8,7 +8,7 @@ fi
 
 # default params
 MAJOR="8.0.0"
-MINOR="4279"
+MINOR="572F"
 ARCH="amd64"
 OSNAME="redhat9"
 
