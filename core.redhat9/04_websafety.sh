0004e50db52cfde715a3cc6a1ab8ffbf3aff4ede
Rafael Akchurin <rafael.akchurin@diladele.com>
2022-09-17 15:16:40 +0200

    8.2

--- core.redhat9/04_websafety.sh
+++ core.redhat9/04_websafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="8.2.0"
-MINOR="2ADA"
+MINOR="871C"
 
 # get latest build
 curl -O https://packages.diladele.com/websafety-core/$MAJOR.$MINOR/amd64/release/redhat9/websafety-${MAJOR}-${MINOR}.x86_64.rpm
