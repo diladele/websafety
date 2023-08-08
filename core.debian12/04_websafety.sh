c61abbc67fc0433106077a9b927e7a1cad7695bd
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-08-08 14:49:23 +0200

    debian 11 -> 12

--- core.debian12/04_websafety.sh
+++ core.debian12/04_websafety.sh
@@ -17,7 +17,7 @@ if [ $? -eq 0 ]; then
     ARCH="armhf"
 fi
 
-wget https://packages.diladele.com/websafety-core/$MAJOR.$MINOR/$ARCH/release/debian11/websafety-$MAJOR.${MINOR}_$ARCH.deb
+wget https://packages.diladele.com/websafety-core/$MAJOR.$MINOR/$ARCH/release/debian12/websafety-$MAJOR.${MINOR}_$ARCH.deb
 
 # install it
 dpkg --install websafety-$MAJOR.${MINOR}_$ARCH.deb
