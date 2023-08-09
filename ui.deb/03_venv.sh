7acf5c4e18c96cb03fa70a662749c340d71815e0
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-08-09 14:43:13 +0200

    different venv commands on ubuntu 20 and debian 12

--- ui.deb/03_venv.sh
+++ ui.deb/03_venv.sh
@@ -7,7 +7,14 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # install various required python packages from the system repo
-apt install -y python3.8-venv python3-dev libjpeg-dev zlib1g-dev libldap2-dev libsasl2-dev libssl-dev
+apt install -y python3-dev libjpeg-dev zlib1g-dev libldap2-dev libsasl2-dev libssl-dev
+
+# install different command on debian 12 or ubuntu
+if [ -f "/etc/lsb-release" ]; then
+   apt install -y python3.8-venv
+else
+   apt install -y python3.11-venv
+fi
 
 # create a virtual environment in the /opt/websafety-ui folder
 python3 -m venv /opt/websafety-ui/env
