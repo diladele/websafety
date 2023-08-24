12ea94ff11b4aa0fa033f445ac012eda4591a470
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-08-24 10:00:46 +0200

    chpass for aws

--- appliance/aws/chpass_aws.sh
+++ appliance/aws/chpass_aws.sh
@@ -17,7 +17,7 @@ else
     NEWPASW=`curl http://169.254.169.254/latest/meta-data/instance-id`
 
     # update the password in the database 
-    sudo -u websafety python3 /opt/websafety-ui/var/console/reset_password.py --password=$NEWPASW
+    sudo -u websafety /opt/websafety-ui/env/bin/python3 /opt/websafety-ui/var/console/reset_password.py --password=$NEWPASW
 
     # change the template too so that user known what shall be used as password
     sudo -u websafety sed -i "s/Passw0rd/InstanceID/g" /opt/websafety-ui/var/console/frame/templates/login.html
@@ -31,7 +31,7 @@ else
 
     echo "If you remove this flag file, the /etc/systemd/service/chpass_aws.service service "  >> "$FLAG_FILE"
     echo "will automatically call /opt/websafety/bin/chpass_aws.sh file that will reset"       >> "$FLAG_FILE"
-    echo "the password of the root user in Web Safety UI to ID of this instance."              >> "$FLAG_FILE"
+    echo "the password of the admin user in Web Safety UI to ID of this instance."              >> "$FLAG_FILE"
     echo "This is required for VMs generated from AMI template in Amazon AWS."                 >> "$FLAG_FILE"
     echo "" >> "$FLAG_FILE"
     echo "" >> "$FLAG_FILE"
