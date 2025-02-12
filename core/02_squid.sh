9e85919b2643e6d993cee94017fb2da8071bd1dc
Rafael Akchurin <rafael.akchurin@diladele.com>
2025-02-12 13:53:36 +0100

    squid 6.13

--- core/02_squid.sh
+++ core/02_squid.sh
@@ -10,8 +10,8 @@ fi
 wget -qO - https://packages.diladele.com/diladele_pub.asc | sudo apt-key add -
 
 # add new repo
-echo "deb https://squid612.diladele.com/ubuntu/ jammy main" \
-   > /etc/apt/sources.list.d/squid612.diladele.com.list
+echo "deb https://squid613.diladele.com/ubuntu/ jammy main" \
+   > /etc/apt/sources.list.d/squid613.diladele.com.list
 
 # and install
 apt-get update && apt-get install -y \
