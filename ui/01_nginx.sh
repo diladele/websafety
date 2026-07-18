1f32a52dc07e5d5eefcb273e063114dbce3a84f3
Rafael Akchurin <rafael.akchurin@diladele.com>
2026-07-18 19:00:32 +0200

    9.8

--- ui/01_nginx.sh
+++ ui/01_nginx.sh
@@ -11,15 +11,3 @@ export DEBIAN_FRONTEND=noninteractive
 
 # install nginx and kerberos client libraries
 apt -y install nginx krb5-user
-
-# install apache and mod_wsgi and some other useful programs
-# apt -y install apache2 libapache2-mod-wsgi-py3
-
-# create the override folder for apache
-# mkdir -p /etc/systemd/system/apache2.service.d/
-
-# override security defaults for Apache introduced in Ubuntu 26.04
-# cat >/etc/systemd/system/apache2.service.d/override.conf << EOL
-# [Service]
-# ProcSubset=all
-# EOL
